0c4d4498308596c50568c2f1af944088c22cca1b
Carra78 <carra78@hotmail.com>
2018-02-28 14:50:51 +0000

    Added additional properties and string override to BookingElement object and added method to call uspRetrieveExistingBooking to DBMethods

--- EllensBnB/EllensCode/BookingElement.cs
+++ EllensBnB/EllensCode/BookingElement.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace EllensBnB.EllensCode
@@ -12,6 +13,16 @@ namespace EllensBnB.EllensCode
 		public DateTime ReservationDate { get; set; }
 		public int NumberOfGuests { get; set; }
 		public decimal RoomRate { get; set; }
+
+		//added for retrieve BookingExistingBooking as returns all details
+		public DateTime DateBookingCreated { get; set; }
+		public string Paid { get; set; }
+		public string BookingNotes { get; set; }
+		public string RoomName { get; set; }
+		public string CustomerName { get; set; }
+		public string CustomerCountry { get; set; }
+		public string CustomerEmail { get; set; }
+		public string CustomerPhone { get; set; }
 
 		public BookingElement(int bookingID, int roomID, DateTime reservedDate, int noOfGuests, decimal roomRate)
 		{
@@ -28,6 +39,42 @@ namespace EllensBnB.EllensCode
 			RoomID = roomID;
 			ReservationDate = reservedDate;
 			NumberOfGuests = noOfGuests;
+		}
+
+		public BookingElement()
+		{
+		}
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append(ReservationDate);
+			sb.Append(", ");
+			sb.Append(BookingID);
+			sb.Append(", ");
+			sb.Append(DateBookingCreated);
+			sb.Append(", ");
+			sb.Append(Paid);
+			sb.Append(", ");
+			sb.Append(RoomID);
+			sb.Append(", ");
+			sb.Append(NumberOfGuests);
+			sb.Append(", ");
+			sb.Append(RoomRate);
+			sb.Append(", ");
+			sb.Append(BookingNotes);
+			sb.Append(", ");
+			sb.Append(RoomName);
+			sb.Append(", ");
+			sb.Append(CustomerName);
+			sb.Append(", ");
+			sb.Append(CustomerCountry);
+			sb.Append(", ");
+			sb.Append(CustomerEmail);
+			sb.Append(", ");
+			sb.Append(CustomerPhone);
+
+			return sb.ToString();
 		}
 	}
 }
